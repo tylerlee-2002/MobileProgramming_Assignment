5ee808abbf1fc7fab84c5d3750d79ecfbc09bd68
TyleR_123 <kimfai0101@icloud.com>
2023-02-21 22:53:05 +0800

    certificate

--- app/src/main/java/com/example/mobileprogramming_assignment/CertActivity.java
+++ app/src/main/java/com/example/mobileprogramming_assignment/CertActivity.java
@@ -1,21 +1,39 @@
 package com.example.mobileprogramming_assignment;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.Objects;
 
 public class CertActivity extends AppCompatActivity {
 
     FirebaseUser mUser;
     Button btnBack, btnDownload;
     String userID, name, email, phoneNumber;
+    int progress, completeUntil;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cert);
+
+
+
 
     }
 }
