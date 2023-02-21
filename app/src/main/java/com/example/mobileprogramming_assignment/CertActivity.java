156bcade746dfab00dad4fbe4444f853448e606c
TyleR_123 <kimfai0101@icloud.com>
2023-02-21 14:20:28 +0800

    popup window

--- app/src/main/java/com/example/mobileprogramming_assignment/CertActivity.java
+++ app/src/main/java/com/example/mobileprogramming_assignment/CertActivity.java
@@ -1,9 +1,17 @@
 package com.example.mobileprogramming_assignment;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class CertActivity extends AppCompatActivity {
+
+    FirebaseUser mUser;
+    Button btnBack, btnDownload;
+    String userID, name, email, phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
