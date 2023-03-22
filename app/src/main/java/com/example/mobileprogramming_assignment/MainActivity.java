2c12d98e8f3ae4a1808da2f16099c0e60d4788e5
RonKu01 <mrku2021@gmail.com>
2023-03-23 06:18:21 +0800

    Course & Quiz left

--- app/src/main/java/com/example/mobileprogramming_assignment/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_assignment/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobileprogramming_assignment;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
@@ -23,6 +24,11 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        @SuppressLint("NonConstantResourceId")
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment;
@@ -38,20 +45,16 @@ public class MainActivity extends AppCompatActivity
                 case R.id.navigationMyProfile:
                     Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    intent.putExtra("userID", userID);
-//                    intent.putExtra("name", name);
-//                    intent.putExtra("phoneNumber", phoneNumber);
-//                    intent.putExtra("email", email);
+                    intent.putExtra("userID", userID);
                     startActivity(intent);
                     overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out);
                     return true;
                 case R.id.navigationMyCourses:
                     return true;
                 case R.id.navigationHome:
+                    // Did Nothing, because already at home page
                     return true;
-                case  R.id.navigationQuiz:
-                    return true;
-                case  R.id.navigationMenu:
+                case R.id.navigationMenu:
                     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                     drawer.openDrawer(GravityCompat.START);
                     return true;
@@ -60,10 +63,15 @@ public class MainActivity extends AppCompatActivity
         }
     };
 
+    String userID;
+    FirebaseUser mUser;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -78,22 +86,75 @@ public class MainActivity extends AppCompatActivity
 
         bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-//
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
         layoutParams.setBehavior(new BottomNavigationBehavior());
-
         bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert mUser != null;
+
+        db.collection("user").whereEqualTo("uid", mUser.getUid()).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    userID = Objects.requireNonNull(document.getData().get("uid")).toString();
+                }
+            }
+        });
 
         androidx.cardview.widget.CardView profileCard = findViewById(R.id.profileCard);
         profileCard.setOnClickListener(v -> {
             Intent intent = new Intent(this, ProfileActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    intent.putExtra("userID", userID);
-//                    intent.putExtra("name", name);
-//                    intent.putExtra("phoneNumber", phoneNumber);
-//                    intent.putExtra("email", email);
+                    intent.putExtra("userID", userID);
             startActivity(intent);
             overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out);
+        });
+
+        androidx.cardview.widget.CardView CourseCard = findViewById(R.id.CourseCard);
+        CourseCard.setOnClickListener(v -> {
+//            Intent intent = new Intent(this, CourseActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.putExtra("userID", userID);
+//            startActivity(intent);
+//            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out);
+        });
+
+        androidx.cardview.widget.CardView ShareCard = findViewById(R.id.ShareCard);
+        ShareCard.setOnClickListener(v -> {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Learn about Dementia!");
+            String shareMessage = "\nShare Dementia App with your family & friends! \nClick Link below to download\n\n";
+            shareMessage = shareMessage + "https://drive.google.com/file/d/1szAdyMrCKM7haalciPiL0_dayNzypNjW/view?usp=sharing" + BuildConfig.APPLICATION_ID + "\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+        });
+
+        androidx.cardview.widget.CardView LogoutCard = findViewById(R.id.LogoutCard);
+        LogoutCard.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Alert");
+            builder.setMessage("Are you sure you want to logout?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Perform the action when the "Yes" button is clicked
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Perform the action when the "No" button is clicked
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
         });
     }
 
@@ -114,14 +175,18 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.navigationHome) {
-            // Handle the camera action
+            // Did Nothing, because already at home page
         } else if (id == R.id.navigationMyCourses) {
 
-        } else if (id == R.id.navigationQuiz) {
-
-        } else if (id == R.id.navigationMyProfile) {
-
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.navigationMyProfile)
+        {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra("userID", userID);
+            startActivity(intent);
+            overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out);
+        } else if (id == R.id.nav_share)
+        {
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
             shareIntent.setType("text/plain");
             shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Learn about Dementia!");
@@ -130,7 +195,8 @@ public class MainActivity extends AppCompatActivity
             shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
             startActivity(Intent.createChooser(shareIntent, "choose one"));
 
-        } else if (id == R.id.btnLogout) {
+        } else if (id == R.id.btnLogout)
+        {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Alert");
                 builder.setMessage("Are you sure you want to logout?");
--- app/src/main/java/com/example/mobileprogramming_assignment/ProfileActivity.java
+++ app/src/main/java/com/example/mobileprogramming_assignment/ProfileActivity.java
@@ -1,85 +1,217 @@
 package com.example.mobileprogramming_assignment;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
+import java.util.Objects;
 
-import java.util.HashMap;
-import java.util.Map;
+public class ProfileActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-public class ProfileActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigationMyProfile:
+                    // Did Nothing, because already at profile page
+                    return true;
+                case R.id.navigationMyCourses:
+                    return true;
+                case R.id.navigationHome:
+                    Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out);
+                    return true;
+                case  R.id.navigationMenu:
+                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                    drawer.openDrawer(GravityCompat.START);
+                    return true;
+            }
+            return false;
+        }
+    };
 
-
-    String userID, name, phoneNumber, email;
-    TextView txtName, txtEmail, txtPhoneNumber;
-    EditText inputPhoneNumber;
-    Button btnConfirm;
-    ImageButton btnClose;
+    String userID, name, email, gender;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    TextView txtName, txtEmail, txtPhoneNumber;
+//    EditText inputPhoneNumber;
+//    Button btnConfirm;
+//    ImageButton btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
+        layoutParams.setBehavior(new BottomNavigationBehavior());
+
+        bottomNavigationView.setSelectedItemId(R.id.navigationMyProfile);
 
         userID = getIntent().getStringExtra("userID");
-        name = getIntent().getStringExtra("name");
-        phoneNumber = getIntent().getStringExtra("phoneNumber");
-        email = getIntent().getStringExtra("email");
 
-        txtName = findViewById(R.id.txtName);
-        txtName.setText(String.format("Name: %s", name));
+        db.collection("user").whereEqualTo("uid", userID).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    userID = Objects.requireNonNull(document.getData().get("uid")).toString();
+                    name = Objects.requireNonNull(document.getData().get("name")).toString();
+                    email = Objects.requireNonNull(document.getData().get("email")).toString();
+                    gender = Objects.requireNonNull(document.getData().get("gender")).toString();
+                }
+            }
+        });
 
-        txtEmail = findViewById(R.id.txtEmail);
-        txtEmail.setText(String.format("Email: %s", email));
+//        txtName = findViewById(R.id.txtName);
+//        txtName.setText(String.format("Name: %s", name));
+//
+//        txtEmail = findViewById(R.id.txtEmail);
+//        txtEmail.setText(String.format("Email: %s", email));
+//
+//        txtPhoneNumber = findViewById(R.id.txtPhoneNumber);
+//        txtPhoneNumber.setText(String.format("Tel: %s", phoneNumber));
+//
+//        btnClose = findViewById(R.id.btnClose);
+//        btnClose.setOnClickListener(v -> {
+//            Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(intent);
+//            overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out);
+//        });
+//
+//        inputPhoneNumber = findViewById(R.id.inputPhoneNumber);
+//        btnConfirm = findViewById(R.id.btnConfirm);
+//        btnConfirm.setOnClickListener(v -> {
+//            String phoneNumber = inputPhoneNumber.getText().toString();
+//
+//            if (phoneNumber.isEmpty() || phoneNumber.length() < 10) {
+//                inputPhoneNumber.setError("Enter Correct Phone Number");
+//            } else {
+//                Map<String, Object> updates2 = new HashMap<>();
+//                updates2.put("phoneNumber", phoneNumber);
+//                FirebaseFirestore.getInstance().collection("user").document(userID).update(updates2).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        Toast.makeText(ProfileActivity.this, "New Phone Number Updated!", Toast.LENGTH_SHORT).show();
+//                        startActivity(intent);
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(ProfileActivity.this, "Error updating document!", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//            }
+//        });
+    }
 
-        txtPhoneNumber = findViewById(R.id.txtPhoneNumber);
-        txtPhoneNumber.setText(String.format("Tel: %s", phoneNumber));
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-        btnClose = findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(v -> {
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.navigationHome) {
             Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out);
-        });
+            return true;
+        } else if (id == R.id.navigationMyCourses) {
 
-        inputPhoneNumber = findViewById(R.id.inputPhoneNumber);
-        btnConfirm = findViewById(R.id.btnConfirm);
-        btnConfirm.setOnClickListener(v -> {
-            String phoneNumber = inputPhoneNumber.getText().toString();
+        } else if (id == R.id.navigationMyProfile) {
+            // Did Nothing, because already at profile page
+        } else if (id == R.id.nav_share)
+        {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Learn about Dementia!");
+            String shareMessage = "\nShare Dementia App with your family & friends! \nClick Link below to download\n\n";
+            shareMessage = shareMessage + "https://drive.google.com/file/d/1szAdyMrCKM7haalciPiL0_dayNzypNjW/view?usp=sharing" + BuildConfig.APPLICATION_ID + "\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
 
-            if (phoneNumber.isEmpty() || phoneNumber.length() < 10) {
-                inputPhoneNumber.setError("Enter Correct Phone Number");
-            } else {
-                Map<String, Object> updates2 = new HashMap<>();
-                updates2.put("phoneNumber", phoneNumber);
-                FirebaseFirestore.getInstance().collection("user").document(userID).update(updates2).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        Toast.makeText(ProfileActivity.this, "New Phone Number Updated!", Toast.LENGTH_SHORT).show();
-                        startActivity(intent);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(ProfileActivity.this, "Error updating document!", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
+        } else if (id == R.id.btnLogout)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Alert");
+            builder.setMessage("Are you sure you want to logout?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Perform the action when the "Yes" button is clicked
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(ProfileActivity.this, SignInActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Perform the action when the "No" button is clicked
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+
+
+
 }
